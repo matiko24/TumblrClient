0d0610a75bb170ed931998bef49750be28ad5cb3
mateuszkomenda <mateusz.komenda@comarch.pl>
2022-03-05 13:12:48 +0100

    UI fixes

--- src/TumblrClient.iOS/Sources/PostsSource.cs
+++ src/TumblrClient.iOS/Sources/PostsSource.cs
@@ -1,5 +1,8 @@
+using System;
 using Foundation;
 using MvvmCross.Platforms.Ios.Binding.Views;
+using TumblrClient.Core.ViewModels;
+using TumblrClient.iOS.Utils;
 using TumblrClient.iOS.Views.Cells;
 using UIKit;
 
@@ -15,6 +18,16 @@ namespace TumblrClient.iOS.Sources
         protected override UITableViewCell GetOrCreateCellFor(UITableView tableView, NSIndexPath indexPath, object item)
         {
             return tableView.DequeueReusableCell(PostViewCell.Key);
+        }
+
+        public override nfloat GetHeightForRow(UITableView tableView, NSIndexPath indexPath)
+        {
+            var item = GetItemAt(indexPath) as PostViewModel;
+            var font = UIFont.SystemFontOfSize(17, UIFontWeight.Regular);
+            var widht = TableView.Frame.Width - 32;
+            var textheight = TextSizer.GetTextHeight(item.Post.Title, widht, font);
+
+            return 90 + textheight;
         }
     }
 }
---
+++ src/TumblrClient.iOS/Utils/TextSizer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using CoreGraphics;
+using Foundation;
+using UIKit;
+
+namespace TumblrClient.iOS.Utils
+{
+    public class TextSizer
+    {
+        public static nfloat GetTextHeight(string text, nfloat widht, UIFont font)
+        {
+            var nsString = new NSString(text);
+            var cgSize = new CGSize(widht, double.MaxValue);
+            var attributes = new UIStringAttributes { Font = font };
+            var textSize = nsString.GetBoundingRect(cgSize, NSStringDrawingOptions.UsesLineFragmentOrigin, attributes, null);
+
+            return (nfloat) Math.Ceiling(textSize.Height);
+        }
+    }
+}
