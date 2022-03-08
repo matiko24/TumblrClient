2064652dc79c22a2fe6f1ea9406505ad316d89aa
mateuszkomenda <mateusz.komenda@comarch.pl>
2022-03-08 01:43:28 +0100

    Missing file

---
+++ src/TumblrClient.Droid/Utils/PostTemplateSelector.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using MvvmCross.DroidX.RecyclerView.ItemTemplates;
+using TumblrClient.Core.Utils;
+using TumblrClient.Core.ViewModels.PostsViewModels;
+
+namespace TumblrClient.Droid.Utils
+{
+    public class PostTemplateSelector : MvxTemplateSelector<PostViewModel>
+    {
+        public override int GetItemLayoutId(int fromViewType)
+        {
+            return fromViewType;
+        }
+
+        protected override int SelectItemViewType(PostViewModel forItemObject)
+        {
+            return forItemObject.Type switch
+            {
+                PostType.Text => Resource.Layout.item_text_post,
+                PostType.Link => Resource.Layout.item_link_post,
+                PostType.Quote => Resource.Layout.item_quote_post,
+                PostType.Photo => Resource.Layout.item_photo_post,
+                _ => throw new NotImplementedException()
+            };
+        }
+    }
+}
